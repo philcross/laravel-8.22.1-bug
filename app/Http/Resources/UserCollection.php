09eee410a2de68b76e02c1d83f414f347ef3f7b4
Phil Cross <p.cross@live.co.uk>
2021-01-15 16:06:29 +0000

    Add comment

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -15,6 +15,9 @@ class UserCollection extends ResourceCollection
      */
     public function toArray($request)
     {
+        // If i was dealing with a single `UserResource` class, then `$this->resource` would point to
+        // the eloquent model, but in a collection, `$this->resource` instead stores an eloquent collection of
+        // user resource objects
         return [
             'users' => $this->collection,
             'all_emails' => $this->resource->map(function (User $user) {
