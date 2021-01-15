fdaa447589d6f4893f11b6701c90156905a6318b
Phil Cross <phil.cross@itccompliance.co.uk>
2021-01-15 10:41:53 +0000

    Initial bug reproduction using framework 8.21.0

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testUserCollection()
+    {
+        User::factory()->count(10)->create();
+
+        $response = $this->getJson('/users');
+
+        $response->assertSuccessful();
+        $response->dump();
+    }
+}
